ca69b00058eddc497f300288060a589347e277b4
Sean W <seanrwhitley@gmail.com>
2022-01-24 12:52:50 -0800

    Namespace 'oops' cleanup

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,5 @@
 import { createElement } from 'lwc';
-import MyApp from 'my/app';
+import MyApp from 'c/app';
 
 const app = createElement('c-app', { is: MyApp });
 // eslint-disable-next-line @lwc/lwc/no-document-query
