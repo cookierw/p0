3d9b66d6bdb57f16bf9aded3fbd9c928cca661f6
Sean W <seanrwhitley@gmail.com>
2022-01-25 22:16:34 -0800

    Base item component

--- src/client/modules/c/item/item.js
+++ src/client/modules/c/item/item.js
@@ -1,3 +1,6 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class Item extends LightningElement {}
+export default class Item extends LightningElement {
+
+    @api item;
+}
