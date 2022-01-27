3a8d4221815831f1243eb41710c5fc39a86deafe
Sean W <seanrwhitley@gmail.com>
2022-01-26 20:44:30 -0800

    Proper grid wrap, rename some classes in itemList for clarity

--- src/client/modules/c/data/TestData.js
+++ src/client/modules/c/data/TestData.js
@@ -33,5 +33,32 @@ export const items = [
         price: "$1",
         rating: 5,
         seller: "Belle McIntosh"
+    },
+    {
+        id: "00001",
+        name: "Gravenstein Apple",
+        image: imageString(),
+        description: "Tart, crispy baking apple.",
+        price: "$1",
+        rating: 5,
+        seller: "John Appleseed"
+    },
+    {
+        id: "00002",
+        name: "Fuji Apple",
+        image: imageString(),
+        description: "Sweet, crispy snacking apple.",
+        price: "$1",
+        rating: 4,
+        seller: "Grandma Smith"
+    },
+    {
+        id: "00003",
+        name: "Pink Pearl Apple",
+        image: imageString(),
+        description: "Tart apple with bright pink flesh.",
+        price: "$1",
+        rating: 5,
+        seller: "Belle McIntosh"
     }
 ]
